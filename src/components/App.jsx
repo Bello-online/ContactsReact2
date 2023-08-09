6d3c572caab29be3cee3332252d96f6195bf7185
Olaseni Bello <Olasenibello38@gmail.com>
2023-08-09 07:53:19 -0400

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://media.licdn.com/dms/image/D5603AQE9zua-16VHfw/profile-displayphoto-shrink_800_800/0/1671503250666?e=2147483647&v=beta&t=mz4jkr8mjAGQzdFk0RvBkqlxVrgmmj5Q0C5u26ssj6A" />
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
+    </div>
+  );
+}
+
+export default App;
